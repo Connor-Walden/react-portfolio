6cd64c071f6681295cd10a7f0b866c2fc57a3c00
Connor-Walden <connorwalden1403@gmail.com>
2021-05-04 16:13:40 +0930

    routing history

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Work from './pages/Work';
 
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
     <div className="App bg-dark">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/work" component={Work}/>
-        </Switch>
+      <Router history={history}>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/work" component={Work}/>
       </Router>
     </div>
   );
