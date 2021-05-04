fa355d875e58c9b3b12224af1e0ba19c5f09bb2d
Connor-Walden <connorwalden1403@gmail.com>
2021-05-04 16:20:01 +0930

    removed browser history

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import { Route, HashRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 import Work from './pages/Work';
 
-import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
-
 function App() {
   return (
     <div className="App bg-dark">
-      <Router history={history}>
+      <Router>
         <Route exact path="/" component={Home}/>
         <Route exact path="/work" component={Work}/>
       </Router>
