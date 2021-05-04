7b17740470569641610fb0e0c645b8778f23e6e8
Connor-Walden <connorwalden1403@gmail.com>
2021-05-04 16:25:57 +0930

    g

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, HashRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Work from './pages/Work';
@@ -6,9 +6,11 @@ import Work from './pages/Work';
 function App() {
   return (
     <div className="App bg-dark">
-      <Router>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/work" component={Work}/>
+      <Router homepage="https://connor-walden.github.io/react-portfolio/">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
+        </Switch>
       </Router>
     </div>
   );
